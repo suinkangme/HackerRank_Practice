352b2a61e1d2b040ac1e77b16f9a66ee85cda5a7
Suin Kang <69102083+suinkangme@users.noreply.github.com>
2023-02-02 14:31:33 -0500

    Create 36_Weather Observation Station 11.sql

---
+++ SQL/EASY/36_Weather Observation Station 11.sql
@@ -0,0 +1,14 @@
+
+SELECT DISTINCT city
+FROM station
+WHERE (city NOT LIKE 'a%'
+AND city NOT LIKE 'e%'
+AND city NOT LIKE 'i%'
+AND city NOT LIKE 'o%'
+AND city NOT LIKE 'u%')
+
+OR (city NOT LIKE '%a'
+AND city NOT LIKE '%e'
+AND city NOT LIKE '%i'
+AND city NOT LIKE '%o'
+AND city NOT LIKE '%u')
