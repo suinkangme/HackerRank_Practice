7b86f0a230fa901c7cb95aaefa9bce8ee496f27c
Suin Kang <69102083+suinkangme@users.noreply.github.com>
2023-02-02 14:16:38 -0500

    Update 26_Weather Observation Station 8.sql

--- SQL/EASY/26_Weather Observation Station 8.sql
+++ SQL/EASY/26_Weather Observation Station 8.sql
@@ -1,1 +1,14 @@
 
+SELECT city
+FROM station
+where (city LIKE 'a%'
+OR city LIKE 'e%'
+OR city LIKE 'i%'
+OR city LIKE 'o%'
+OR city LIKE 'u%')
+
+AND (city LIKE '%a'
+OR city LIKE '%e'
+OR city LIKE '%i'
+OR city LIKE '%o'
+OR city LIKE '%u');
